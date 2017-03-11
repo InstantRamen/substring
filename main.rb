39418f207064b61dc6bf09017abbe6a7ea8a1ac6
Derrick Cudd <instantramen125@gmail.com>
2017-03-11 02:14:04 +0000

    finish substring method

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 my_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "hell", "hello"]
-string_to_use = "Hello, from below"
+string_to_use = "Hello, from below low, lowsy help hellp how do you do howdy"
  
 def substring(dictionary, string)
   string_array = string.split(" ")
@@ -8,9 +8,17 @@ def substring(dictionary, string)
   dictionary.each do |word_in_dictionary|
     string_array.each do |word_in_string|
       word_in_string = word_in_string.downcase.gsub(/[^a-z0-9\s]/i, '')
-      puts word_in_dictionary if word_in_string.match(/#{word_in_dictionary}/) and word_in_dictionary != word_in_string
+      sub = word_in_dictionary if word_in_string.match(/#{word_in_dictionary}/)
+      if sub != nil
+        if substrings[sub] == nil
+          substrings[sub] = 1
+        else
+          substrings[sub] += 1
+        end
+      end
     end
   end
+  return substrings
 end
 
-substring(my_dictionary, string_to_use)
+puts substring(my_dictionary, string_to_use)
